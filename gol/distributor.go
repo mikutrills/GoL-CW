dc4d6c1b2940c3e164fa4103b43856a257b4e719
ikmaalk <ikmaalkasim@gmail.com>
2022-11-22 23:26:19 +0000

    trial

--- gol/distributor.go
+++ gol/distributor.go
@@ -13,21 +13,19 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
-
+	// this is a trial test
 	turn := 0
-
 
 	// TODO: Execute all turns of the Game of Life.
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
